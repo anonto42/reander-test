64e616073aa4668905918c2395658d5c1eb20973
Sohidul Islam Ananto <anontom90@gmail.com>
2024-09-04 12:52:24 +0600

    upsh

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ app.get('/test',(req, res) => {
     res.json({ message: 'Hello, World!' });
 })
 
-app.listen(3760,() => console.log(`Server is running on port 3000`));
+app.listen(3760,() => console.log(`Server is running on port 3760`));
